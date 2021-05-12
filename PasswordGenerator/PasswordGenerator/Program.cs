cf29631f97f70394c5649e6888c64299dbd5adb1
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-05-12 11:01:34 +0200

    Created Project - First Task 1

---
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Options");
+            Console.WriteLine("- l  = lower case letter");
+            Console.WriteLine("- L  = upper case letter");
+            Console.WriteLine("- d  = digit");
+            Console.WriteLine("- s  = special character ( !#¤%&/(){}[] )");
+
+            Console.WriteLine("Example: PasswordGenerator 14 lLssdd");
+            Console.WriteLine("Min. 1 lower case");
+            Console.WriteLine("Min. 1 upper case");
+            Console.WriteLine("Min. 2 special characters");
+            Console.WriteLine("Min. 2 digits");
+            Console.ReadLine();
+        }
+    }
+}
